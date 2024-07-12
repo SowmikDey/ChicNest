9340d99a5d871119b0d3c5266261f15785bea21a
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-07-12 22:13:07 +0530

    commit
    new work in tech.jsx

--- client/src/Tech-components/Navbar1.jsx
+++ client/src/Tech-components/Navbar1.jsx
@@ -1,25 +1,49 @@
 // Navbar1.js
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar1.css";
 
 const Navbar1 = () => {
+  const [show, setShow] = useState(false);
+
+  const showDropdown = () => {
+    setShow(true);
+  };
+
+  const hideDropdown = () => {
+    setShow(false);
+  };
+
   return (
     <div>
       <div className="n">
-        <i className="fa-solid fa-bars"></i>
+        <button className="icon-button black" onClick={showDropdown}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        {show && (
+          <div className="dropdown">
+            <ul>
+              <a href="/tech"><li>TECH</li></a>
+              <a href="/food"><li>FOOD</li></a>
+              <a href="/clothes"><li>CLOTHES</li></a>
+            </ul>
+          </div>
+        )}
+        <button className="icon-button red" onClick={hideDropdown}>
+          <i className="fa-solid fa-eject"></i>
+        </button>
       </div>
       <div className="nav">
         <h2 className="h">CHICNEST</h2>
         <div className="links">
-          <a href="">Tech1</a>
-          <a href="">Tech2</a>
-          <a href="">Tech3</a>
-          <a href="">Tech4</a>
+          <a href="#tech1">Tech1</a>
+          <a href="#tech2">Tech2</a>
+          <a href="#tech3">Tech3</a>
+          <a href="#tech4">Tech4</a>
         </div>
         <hr />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar1;
